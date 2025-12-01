97231a1bb31fef78dfe5c25b8fec4f437de36ef1
Yehuda Katz <wycats@gmail.com>
2025-12-01 12:38:50 -0800

    infra: switch deployment to Vercel and update domain to axioms.design

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -12,6 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://axioms.design",
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeMathjax],
